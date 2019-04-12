6c9d432fd9f88b40c268c62c1efd6076472bdc0d
Burak Sezer <burak.sezer.developer@gmail.com>
2019-04-12 18:25:49 +0300

    Create a Spring Boot project

---
+++ src/main/java/com/olriclabs/chapstream/ChapstreamApplication.java
@@ -0,0 +1,13 @@
+package com.olriclabs.chapstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChapstreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChapstreamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/olriclabs/chapstream/ChapstreamApplicationTests.java
@@ -0,0 +1,16 @@
+package com.olriclabs.chapstream;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChapstreamApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
